d4c08d71980db88cb153bf2b653e7c969e975d83
faroke <faroke-wb@outlook.fr>
2017-04-08 12:25:41 +0200

    Update AutoBackupFT.sh

--- AutoBackupFT.sh
+++ AutoBackupFT.sh
@@ -3,6 +3,10 @@
 #Remastersys auto-install
 cd
 echo "Selectionne ton répertoire d'utilisateur par defaut : " 
-read -p "{Exemple: /home/monutilisateur ; /root} > " user_def
+read -p "{Exemple: /home/monutilisateur ; /root} > " user
 rm -rf /etc/skel
 cp -R $user /etc/skel
+sudo add-apt-repository ppa:mutse-young/remastersys -y
+sudo apt-get update -y
+sudo apt-get install remastersys remastersys-gtk -y
+read -p "Voulez vous faire votre Backup.iso hors connectin? (Y/N) >" connectedornot
