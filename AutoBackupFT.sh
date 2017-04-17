e4bb0f63dc2dac2727fba6ead7e49cedd018e032
faroke <faroke-wb@outlook.fr>
2017-04-17 10:38:54 +0200

    Update AutoBackupFT.sh

--- AutoBackupFT.sh
+++ AutoBackupFT.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 # GNU GENERAL PUBLIC LICENSE
-#Remastersys auto-install
+#Respin Auto-Install
 
-##REGROUPER TT SA DANS UN MODULE DE SELECTION
-maker () {
-sudo respin clean
-sudo respin backup
-}
-cd
 echo "Selectionne ton répertoire d'utilisateur par defaut : " 
 read -p "{Exemple: /home/monutilisateur ; /root} > " user
 sudo rm -rf /etc/skel
@@ -17,17 +11,5 @@ sudo add-apt-repository ppa:sergiomejia666/respin -y
 sudo add-apt-repository ppa:sergiomejia666/xresprobe -y
 sudo apt-get update 
 sudo apt-get install xresprobe respin -y
-read -p "Voulez vous faire votre Backup.iso hors connectin? (Y/N) >" connectedornot
-if [ connectedornot ="Y" ]; then
-maker
-exit
-else
-if [ connectedornot ="N" ]; then
-##sudo apt-get install -y ubiquity-frontend-KDE
-sudo apt-get install -y ubiquity-frontend-gtk
-exit
-else
-##BOUCLE A FAIRE
-exit
-fi
-fi
+sudo respin clean
+sudo respin backup
