74b2952d27a5bd7457f5d2b09b7c20ab5083634f
faroke <faroke-wb@outlook.fr>
2017-04-17 10:33:01 +0200

    Update AutoBackupFT.sh

--- AutoBackupFT.sh
+++ AutoBackupFT.sh
@@ -4,17 +4,19 @@
 
 ##REGROUPER TT SA DANS UN MODULE DE SELECTION
 maker () {
-sudo remastersys clean
-sudo remastersys backup
+sudo respin clean
+sudo respin backup
 }
 cd
 echo "Selectionne ton répertoire d'utilisateur par defaut : " 
 read -p "{Exemple: /home/monutilisateur ; /root} > " user
 sudo rm -rf /etc/skel
 sudo cp -R $user /etc/skel
-sudo add-apt-repository ppa:mutse-young/remastersys -y
-sudo apt-get update -y
-sudo apt-get install remastersys remastersys-gtk -y
+sudo apt-get install isolinux
+sudo add-apt-repository ppa:sergiomejia666/respin -y
+sudo add-apt-repository ppa:sergiomejia666/xresprobe -y
+sudo apt-get update 
+sudo apt-get install xresprobe respin -y
 read -p "Voulez vous faire votre Backup.iso hors connectin? (Y/N) >" connectedornot
 if [ connectedornot ="Y" ]; then
 maker
