4a9d10118afd48a6f6d78cf62dcac2e9fafc5abb
faroke <faroke-wb@outlook.fr>
2017-04-17 18:59:49 +0200

    Update AutoBackupFT.sh

--- AutoBackupFT.sh
+++ AutoBackupFT.sh
@@ -6,7 +6,7 @@ echo "Selectionne ton répertoire d'utilisateur par defaut : "
 read -p "{Exemple: /home/monutilisateur ; /root} > " user
 sudo rm -rf /etc/skel
 sudo cp -R $user /etc/skel
-sudo apt-get install isolinux
+sudo apt-get install isolinux -y
 sudo add-apt-repository ppa:sergiomejia666/respin -y
 sudo add-apt-repository ppa:sergiomejia666/xresprobe -y
 sudo apt-get update 
